cd51bf89e0992bd72d4f833da11b9a17ece03842
dashengz <contact@dashengz.com>
2017-03-29 18:37:14 -0400

    Added menu

--- app/src/main/java/com/dashengz/inclassassignment08_dashengz/MainActivity.java
+++ app/src/main/java/com/dashengz/inclassassignment08_dashengz/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -95,7 +98,26 @@ public class MainActivity extends AppCompatActivity {
         name.setText("");
     }
 
-    public void logout(View view) {
+    public void logout() {
         mAuth.signOut();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
